7d7349792858ce55ca6e9a5e56d3281f144c2500
Mariusz Kolodziej <makolo@microsoft.com>
2018-06-18 00:44:00 -0400

    updating build image task

--- tasks/build-image-task.sh
+++ tasks/build-image-task.sh
@@ -14,6 +14,8 @@ echo 'Build packer template'
 #packer build -var-file='../image-configuration/config.json' -var-file='../image-secret/secrets.json' template.json
 
 echo 'Copy manifest.json to artifact repo'
-cd ../image-artifact
+cd ../
+cp image-rhel7/manifest.json image-artifact/manifest.json
+cd image-artifact
 git status
 
