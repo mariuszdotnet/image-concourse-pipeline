5a73797b945336c98b88c78a7b8e8bf59179db3b
Mariusz Kolodziej <makolo@microsoft.com>
2018-06-17 22:36:46 -0400

    updating build image task

--- tasks/build-image-task.sh
+++ tasks/build-image-task.sh
@@ -8,4 +8,4 @@ cd image-rhel7
 ls -al
 
 echo 'Validate packer template'
-packer validate -var-file='../image-configuration/config.json' -var-file='../image-secret/secret.json' template.json
+packer validate -var-file='../image-configuration/config.json' -var-file='../image-secret/secrets.json' template.json
