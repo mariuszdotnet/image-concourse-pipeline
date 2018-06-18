71295cbf3bdda319b0e4de990b13528d554571fd
Mariusz Kolodziej <makolo@microsoft.com>
2018-06-17 22:40:11 -0400

    updating build image task

--- tasks/build-image-task.sh
+++ tasks/build-image-task.sh
@@ -9,3 +9,6 @@ ls -al
 
 echo 'Validate packer template'
 packer validate -var-file='../image-configuration/config.json' -var-file='../image-secret/secrets.json' template.json
+
+echo 'Build packer template'
+packer build -var-file='../image-configuration/config.json' -var-file='../image-secret/secrets.json' template.json
