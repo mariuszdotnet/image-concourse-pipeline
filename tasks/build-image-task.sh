e16ae75dd75417fb1cf1f8956223e4df3b1cef8b
Mariusz Kolodziej <makolo@microsoft.com>
2018-06-18 21:00:41 -0400

    end to end test

--- tasks/build-image-task.sh
+++ tasks/build-image-task.sh
@@ -11,7 +11,7 @@ echo 'Validate packer template'
 packer validate -var-file='../image-configuration/config.json' -var-file='../image-secret/secrets.json' template.json
 
 echo 'Build packer template'
-#packer build -var-file='../image-configuration/config.json' -var-file='../image-secret/secrets.json' template.json
+packer build -var-file='../image-configuration/config.json' -var-file='../image-secret/secrets.json' template.json
 
 echo 'Copy manifest.json to artifact repo'
 cd ../
@@ -19,12 +19,13 @@ git clone image-artifact image-artifact-output
 cp image-rhel7/manifest.json image-artifact-output/manifest.json
 cd image-artifact-output
 
+
 git status
 
 echo 'Commit new manifest file'
 git config --global user.email "nobody@concourse-ci.org"
 git config --global user.name "Concourse"
 git add .
-git commit -m "Published packer manifest file"
+git commit -m "Commit packer manifest file"
 
 echo 'Job completed!'
